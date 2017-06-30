34da849ec23503682881309169f718970ad9485d
Macgyver Martins <macgyver.martins@gmail.com>
2017-06-30 08:22:52 -0400

    git init and inir react app

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <p className="App-intro">
+          <h1> Bem-Vindo Macgyver</h1>
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
